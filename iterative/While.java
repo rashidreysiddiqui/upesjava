632e1e453713512d46eb5730b4d212c3c8f8968a
rashid <rashidsiddqui07@gmail.com>
2020-05-07 21:00:27 +0530

    iterative while do while

---
+++ iterative/While.java
@@ -0,0 +1,46 @@
+class While{
+
+
+public static void main(String args[]){
+
+// true>false
+// boolean-> true, false 
+
+// while loop will alwways take boolean value 
+// while(true)
+// System.out.println("hello wworld"); 
+// while(true); -> noerror
+// while(true)
+// int x=10;
+// while(true)
+// {
+// int x=10;
+
+// }
+
+ 
+//  final int x=10;
+//  final int y=20;
+// //  int z=x+y;
+//  while(x>y)
+// {
+//     System.out.println("hello");
+// }
+//    System.out.println("rashid");
+// }
+
+
+// }
+// statement-> empty if (){}
+// statement -> compund statement if(){ -------  -------------- ------------}
+
+// dowhile->
+ int x=10,y=20;
+do { System.out.println("statment !!!!");  }
+while(x>y);
+System.out.println("outside !!!!");
+
+}}
+
+
+
